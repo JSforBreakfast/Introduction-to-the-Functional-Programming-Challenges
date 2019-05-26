3403c3a040716d9b58f4c37d31b66e3caaf54bc0
Vitor Emanuel <veslima3@gmail.com>
2019-05-26 00:16:33 -0300

    Combine Two Arrays Using the concat Method
    Concatenation means to join items end to end. JavaScript offers the concat method for both strings and arrays that work in the same way. For arrays, the method is called on one, then another array is provided as the argument to concat, which is added to the end of the first array. It returns a new array and does not mutate either of the original arrays. Here's an example:
    
    [1, 2, 3].concat([4, 5, 6]);
    // Returns a new array [1, 2, 3, 4, 5, 6]
    
    Use the concat method in the nonMutatingConcat function to concatenate attach to the end of original. The function should return the concatenated array.

---
+++ Combine Two Arrays Using the concat Method.js
@@ -0,0 +1,11 @@
+let first = [1, 2, 3];
+let second = [4, 5];
+
+function nonMutatingConcat(original, attach) {
+  // Add your code below this line
+ return first.concat(second);
+  
+  // Add your code above this line
+}
+
+nonMutatingConcat(first, second);
