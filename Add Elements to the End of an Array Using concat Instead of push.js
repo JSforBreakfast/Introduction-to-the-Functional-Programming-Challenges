d821a7e42766bca72d8e138fae21015e0512ccae
Vitor Emanuel <veslima3@gmail.com>
2019-05-26 01:04:45 -0300

    Add Elements to the End of an Array Using concat Instead of push
    Functional programming is all about creating and using non-mutating functions.
    
    The last challenge introduced the concat method as a way to combine arrays into a new one without mutating the original arrays. Compare concat to the push method. Push adds an item to the end of the same array it is called on, which mutates that array. Here's an example:
    
    var arr = [1, 2, 3];
    arr.push([4, 5, 6]);
    // arr is changed to [1, 2, 3, [4, 5, 6]]
    // Not the functional programming way
    Concat offers a way to add new items to the end of an array without any mutating side effects.
    
    
    Change the nonMutatingPush function so it uses concat to add newItem to the end of original instead of push. The function should return an array.

---
+++ Add Elements to the End of an Array Using concat Instead of push.js
@@ -0,0 +1,11 @@
+let first = [1, 2, 3];
+let second = [4, 5];
+
+function nonMutatingPush(original, newItem) {
+  // Add your code below this line
+  return first.concat(second);
+  
+  // Add your code above this line
+}
+
+nonMutatingPush(first, second);
